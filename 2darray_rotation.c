6b6c63988d232001accfaf3c29b79b4cd3b906ca
Rahul-Chauhan21 <crahul1721@gmail.com>
2019-08-03 11:31:36 +0530

    change made

--- 2darray_rotation.c
+++ 2darray_rotation.c
@@ -1,69 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-# define m 4
-#define n 4
-
-void rotate(int arr[m][n],int row,int column) {
-	int m1=m;
-	int n1=n;
-	int c=0,prev=0;
-	if(row>m || column>n)
+#include<iostream>
+#define ROTATIONS 2
+#define M 4
+#define N 4
+using namespace std;
+void rotate(int row,int col,int arr[M][N],int n1,int m1){
+	int curr=0,prev=0;
+	if(row>M||col>N)
 		return;
-	if(row+1==m || column+1==n)
+	if(row+1==M|col+1==N)
 		return;
-	c++;
-	if(c==2) {
-		prev=arr[row][column];
-		printf("%d\n",prev);
-	}
-	if(c==1)
-		prev=arr[row+1][column];
-	int curr=0;
-	for(int i=0;i<n;i++) {
+	prev=arr[row+1][col];
+	for(int i=col;i<n1;i++){
 		curr=arr[row][i];
 		arr[row][i]=prev;
 		prev=curr;
 	}
-	row++;
-	for(int i=row;i<n;i++) {
-		curr=arr[i][n-1];
-		arr[i][n-1]=prev;
+	for(int i=row+1;i<n1;i++){
+		curr=arr[i][n1-1];
+		arr[i][n1-1]=prev;
 		prev=curr;
 	}
+	for(int i=n1-2;i>=col;i--){
+		curr=arr[n1-1][i];
+		arr[n1-1][i]=prev;
+		prev=curr;
+	}
+	for(int i=n1-2;i>=row+1;i--){
+		curr=arr[i][col];
+		arr[i][col]=prev;
+		prev=curr;
+	}
+	row++;
+	col++;
 	n1--;
-	for(int i=n1-1;i>=column;i--) {
-		curr=arr[m-1][i];
-		arr[m-1][i]=prev;
-		prev=curr;
-	}
 	m1--;
-	for(int i=m1-1;i>=row;i--) {
-		curr=arr[i][column];
-		arr[i][column]=prev;
-		prev=curr;
-	}
-	row++;
-	column++;
-	rotate(arr,row,column);
+	rotate(row,col,arr,n1,m1);
 }
 
-int main(int argc,char *argv[]) {
-	int arr[m][n],k=1;
-	for(int i=0;i<m;i++) {
-		for(int j=0;j<n;j++) {
-			arr[i][j]=atoi(argv[k]);
-			k++;
+int main(void){
+	int arr[M][N]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+	int n1=N;
+	int m1=M;
+	for(int i=0;i<ROTATIONS;i++)
+		rotate(0,0,arr,n1,m1);
+	for(int i=0;i<M;i++){
+		for(int j=0;j<N;j++){
+			cout<<arr[i][j]<<" ";
 		}
-	}
-//	int r=atoi(argv[argc-1]);
-//	for(int i=0;i<r;i++) {
-		rotate(arr,0,0);
-//	}
-	for(int i=0;i<m;i++) {
-		for(int j=0;j<n;j++) {
-			printf("%d ",arr[i][j]);
-		}
-		printf("\n");
-	}
+		cout<<"\n";
+	}s
 	return 0;
 }
