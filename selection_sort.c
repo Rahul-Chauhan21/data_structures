0c4983258195fc7f2b48f1403113748490a687ad
Rahul-Chauhan21 <crahul1721@gmail.com>
2019-08-09 10:50:12 +0530

    selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -1,36 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
-void swap(int *a,int *b)
+void
+swap (int *a, int *b)
 {
-int temp;
-temp=*a;
-*a=*b;
-*b=temp;
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
-int main(int argc,const char* argv[])
-{
-int n=argc-1,j=0;
-int arr[n];
 
-for(int i=0;i<argc;i++)
+int
+main (int argc, const char *argv[])
 {
-if(i==0)continue;
-arr[j++]=atoi(argv[i]);
+  int n = argc - 1, j = 0;
+  int arr[n];
+
+  for (int i = 0; i < argc; i++)
+    {
+      if (i == 0)
+	continue;
+      arr[j++] = atoi (argv[i]);
+    }
+  int index;
+  for (int i = 0; i < n; i++)
+    {
+      index = i;
+      for (int j = i + 1; j < n; j++)
+	{
+	  if (arr[j] < arr[index])
+	    index = j;
+	}
+      swap (&arr[index], &arr[i]);
+    }
+  for (int i = 0; i < n; i++)
+    {
+      printf ("%d ", arr[i]);
+    }
+  return 0;
 }
-int index;
-for(int i=0;i<n;i++)
-{
-index=i;
-for(int j=i+1;j<n;j++)
-{
-if(arr[j]<arr[index])
-index=j;
-}
-swap(&arr[index],&arr[i]);
-}
-for(int i=0;i<n;i++)
-{
-printf("%d ",arr[i]);
-}
-return 0;
-}
+
