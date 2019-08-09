6b21458527a18e73caab10add15b4e5b16c95b74
Rahul-Chauhan21 <crahul1721@gmail.com>
2019-08-09 10:46:07 +0530

    selection_sort

---
+++ selection_sort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+void swap(int *a,int *b)
+{
+int temp;
+temp=*a;
+*a=*b;
+*b=temp;
+}
+int main(int argc,const char* argv[])
+{
+int n=argc-1,j=0;
+int arr[n];
+
+for(int i=0;i<argc;i++)
+{
+if(i==0)continue;
+arr[j++]=atoi(argv[i]);
+}
+int index;
+for(int i=0;i<n;i++)
+{
+index=i;
+for(int j=i+1;j<n;j++)
+{
+if(arr[j]<arr[index])
+index=j;
+}
+swap(&arr[index],&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+printf("%d ",arr[i]);
+}
+return 0;
+}
